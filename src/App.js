19e711af636decf2dce70c3dbd34240e7f4d84ca
Dhanunjayanaidu Maradana <prasadchandra6817@gmail.com>
2024-07-02 23:11:14 +0530

    ui changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Dhanunjay Naidu Maradana</h1>
+        <h1>Dhanunjay Naidu M</h1>
         <p>UI Designer / Frontend Developer</p>
       </header>
     </div>
