957e3f2d7fed082f7d7d795bdb1596a47fa260b8
Dhanunjayanaidu Maradana <dhanunjayanaidu.maradana@gmail.com>
2024-07-16 20:35:13 +0530

    contact form updated

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -4,17 +4,17 @@ import "./Contact.scss";
 import { HiOutlineMailOpen } from "react-icons/hi";
 import { Fade } from "react-reveal";
 import { Form } from "react-bootstrap";
-import {  Bounce, ToastContainer, toast } from 'react-toastify';
+import { Bounce, ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const Contact = () => {
   const form = useRef();
-  const [name,setName] = useState('')
-  const [email,setEmail] = useState('')
-  const [text,setText] = useState('')
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [text, setText] = useState("");
   const sendEmail = (e) => {
     e.preventDefault();
-    
+
     emailjs
       .sendForm("service_abwtzud", "template_t8gtnxm", form.current, {
         publicKey: "kBDF81UqK-LxVpd4p",
@@ -22,10 +22,10 @@ const Contact = () => {
       .then(
         () => {
           console.log("SUCCESS!");
-          setEmail('')
-          setName('')
-          setText('')
-          toast.success('Email has been sent', {
+          setEmail("");
+          setName("");
+          setText("");
+          toast.success("Email has been sent", {
             position: "top-right",
             autoClose: 5000,
             hideProgressBar: false,
@@ -35,20 +35,20 @@ const Contact = () => {
             progress: undefined,
             theme: "light",
             transition: Bounce,
-            });
+          });
         },
         (error) => {
           console.log("FAILED...", error.text);
-          setEmail('')
-          setName('')
-          setText('')
+          setEmail("");
+          setName("");
+          setText("");
         }
       );
   };
 
   return (
     <>
-      <ToastContainer/>
+      <ToastContainer />
       <section className="Contact" id="Contact">
         <h6 class="subtitle">
           <HiOutlineMailOpen /> Contact
@@ -60,47 +60,55 @@ const Contact = () => {
         </Fade>
         <div>
           <Form ref={form} onSubmit={sendEmail}>
-            <Form.Group className="mb-3" controlId="">
-              <Form.Label>Full Name</Form.Label>
-              <Form.Control
-                type="text"
-                name="user_name"
-                placeholder="John Deo"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                name="user_email"
-                placeholder="your@email.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="">
-              <Form.Label>Example textarea</Form.Label>
-              <Form.Control
-                as="textarea"
-                name="message"
-                rows={5}
-                placeholder="Enter your message"
-                value={text}
-                onChange={(e) => setText(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="">
-              <input class="btn btn-primary px-5" type="submit" value="Send" />
-            </Form.Group>
-            {/* <label>Name</label>
-            <input type="text" name="user_name" />
-            <label>Email</label>
-            <input type="email" name="user_email" />
-            <label>Message</label>
-            <textarea name="message" />
-            <input type="submit" value="Send" /> */}
+            <Fade bottom duration={3000}>
+              <Form.Group className="mb-3" controlId="">
+                <Form.Label>Full Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="user_name"
+                  placeholder="John Deo"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </Form.Group>
+            </Fade>
+            <Fade bottom duration={3000}>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  name="user_email"
+                  placeholder="your@email.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Form.Group>
+            </Fade>
+            <Fade bottom duration={3000}>
+              <Form.Group className="mb-3" controlId="">
+                <Form.Label>Example textarea</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  name="message"
+                  rows={5}
+                  placeholder="Enter your message"
+                  value={text}
+                  onChange={(e) => setText(e.target.value)}
+                />
+              </Form.Group>
+            </Fade>
+            <Fade bottom duration={3000}>
+              <Form.Group className="mb-3" controlId="">
+                <input
+                  class="btn btn-primary px-5"
+                  type="submit"
+                  value="Send"
+                />
+              </Form.Group>
+            </Fade>
           </Form>
         </div>
       </section>
